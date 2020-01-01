97b5d56cbf5d8eddb208ef7df3967db49308bfbd
Roc <zkp9701@163.com>
2020-01-01 18:47:53 +0800

    Update Constant.java

--- src/main/java/com/roc/pojo/Constant.java
+++ src/main/java/com/roc/pojo/Constant.java
@@ -10,5 +10,5 @@ public class Constant {
 
     public static final String  MAP_URL_PREFIX="https://restapi.amap.com/v3/geocode/regeo?location=";
 
-    public static final String MAP_URL_SUFFIX="&key=a6d2df7f27a6e3a5cc28e376062e5bf6&radius=100&extensions=all&batch=false&roadlevel=0";
+    public static final String MAP_URL_SUFFIX="&key=xxxxx&radius=100&extensions=all&batch=false&roadlevel=0";
 }
